4fa5c71ed53ad32d80b31be789494aaf2bf519b2
KooSangWon <gsw0931@gmail.com>
2018-06-21 18:57:07 +0900

    MyPortfoilo

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,52 @@
 <template>
-  <div>
-    <nuxt/>
+  <section class="section">
+    <div class="hero is-medium is-dark is-bold">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-1">구상원의 포트폴리오</h1>
+          <br>
+          <h2 class="subtitle">목원대학교 정보통신공학융합부</h2>
+        </div>
+      </div>
+    </div>
+    <br>
+
+<div class="tabs is-toggle is-fullwidth">
+  <ul>
+    <li class="is-active">
+      <a>
+        <span class="icon is-small"><i class="fas fa-home" aria-hidden="true"></i></span>
+        <span>메인페이지</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span class="icon is-small"><i class="fas fa-user" aria-hidden="true"></i></span>
+        <span>프로필</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span class="icon is-small"><i class="fas fa-book" aria-hidden="true"></i></span>
+        <span>자기개발</span>
+      </a>
+    </li>
+    <li>
+      <a>
+        <span class="icon is-small"><i class="far fa-file-star" aria-hidden="true"></i></span>
+        <span>수상 이력</span>
+      </a>
+    </li>
+  </ul>
+</div>
+
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      made by <strong>Koo Sang Won</strong> <br>Mokwon University<br>E-Portfolio
+    </p>
   </div>
+</footer>
+
+  </section>
 </template>
-
-<style>
-html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
-</style>
